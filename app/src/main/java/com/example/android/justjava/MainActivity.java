279022788fb27035ebe1ea5e02fe623212b899fa
Omar <oa.azab@gmail.com>
2017-03-08 20:02:04 +0200

    Add Whipped Cream CheckBox

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,9 +3,8 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -13,6 +12,9 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
+    boolean hasWhippedCream = false;
+
+    CheckBox mCheckBoxWhippedCream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +28,7 @@ public class MainActivity extends AppCompatActivity {
      * @return total price
      */
     public int calculatePrice() {
-        int price = quantity * 5;
-        return price;
+        return quantity * 5;
     }
 
     /**
@@ -36,11 +37,12 @@ public class MainActivity extends AppCompatActivity {
      * @param price total price of the order
      * @return message of the summary
      */
-    public String createOrderSummary(int price) {
+    private String createOrderSummary(int price, boolean hasWhippedCream) {
         String summaryMessage = "Name: Omar Ahmed";
+        summaryMessage += "\nAdd Whipped cream? " + hasWhippedCream;
         summaryMessage += "\nQuantity: " + quantity;
         summaryMessage += "\nTotal: $" + price;
-        summaryMessage += "\n Thank you!";
+        summaryMessage += "\nThank you!";
         return summaryMessage;
     }
 
@@ -48,7 +50,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String summaryMessage = createOrderSummary(calculatePrice());
+        mCheckBoxWhippedCream = (CheckBox) findViewById(R.id.boxWhippedCream);
+        hasWhippedCream = mCheckBoxWhippedCream.isChecked();
+        String summaryMessage = createOrderSummary(calculatePrice(),hasWhippedCream);
         displayMessage(summaryMessage);
     }
 
@@ -60,20 +64,13 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
     /**
